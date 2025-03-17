c150c392d59d3db4264f7dc78caf83bec9ac9875
Oubaid Beldi <oubaidbeldi@gmail.com>
2025-03-17 04:19:33 +0100

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,45 @@
+import { Text, View ,StyleSheet,Image} from "react-native";
+import PostItImage from '@/assets/images/post-it.png'
+
+const HomeScreen = ()=> {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Image source={PostItImage} style={styles.image}/>
+      <Text style={styles.title}> Welcome To Notes App</Text>
+      <Text style={styles.subtitle}> Capture your thoughts anytime , anywhere </Text>
+    </View>
+  );
+}
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center",
+    padding: 20,
+    // padding:20,
+    backgroundColor:'#f8f9fa'
+    
+  },
+  image:{
+    width:100,
+    height:100,
+    marginBottom:20,
+    borderRadius:10
+  },
+  title:{
+    fontSize:28,
+    fontWeight:'bold',
+    marinBottom:10,
+    color:'#333'
+  },
+  subtitle:{
+    fontSize:16,
+    color:'#666',
+    textAlign:'center',
+    marginBottom:20,
+  }
+
+})
+export default HomeScreen
